c6d208e45cab3fd24d56211f813d9cd026571818
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-03-01 14:17:18 +0100

    Draft basic API client architecture

---
+++ Nodsoft.Wargaming.Api.Client/ApiClientExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Mime;
+using Microsoft.Extensions.DependencyInjection;
+using Nodsoft.Wargaming.Api.Client.Clients;
+
+namespace Nodsoft.Wargaming.Api.Client;
+
+public static class ApiClientExtensions
+{
+	public static IHttpClientBuilder AddApiClient<TClient>(this IServiceCollection services, Action<IServiceProvider, HttpClient> configureClient) 
+		where TClient : class, IApiClient
+	{
+		return services.AddHttpClient<TClient>(configureClient)
+			.ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+			{
+				AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.None
+			});
+	}
+}
---
+++ Nodsoft.Wargaming.Api.Client/Clients/ApiClientBase.cs
@@ -0,0 +1,13 @@
+﻿namespace Nodsoft.Wargaming.Api.Client.Clients;
+
+public abstract class ApiClientBase : IApiClient
+{
+	protected HttpClient Client { get; set; }
+
+	public ApiClientBase(HttpClient client)
+	{
+		Client = client;
+	}
+	
+	
+}
