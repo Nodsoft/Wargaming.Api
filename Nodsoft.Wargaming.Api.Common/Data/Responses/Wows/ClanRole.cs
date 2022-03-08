70eacdc48fbe73948b034cde3cb32aa89b71d4a2
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-03-08 10:12:52 +0100

    Breakout ClanRole conversion methods

--- Nodsoft.Wargaming.Api.Common/Data/Responses/Wows/ClanRole.cs
+++ Nodsoft.Wargaming.Api.Common/Data/Responses/Wows/ClanRole.cs
@@ -10,3 +10,28 @@ public enum ClanRole : byte
 	Officer,
 	Private
 }
+
+public static class ClanRoleExtensions
+{
+	public static ClanRole ParseClanRole(this string? value) => value switch
+	{
+		"commander"            => ClanRole.Commander,
+		"executive_officer"    => ClanRole.ExecutiveOfficer,
+		"recruitment_officer"  => ClanRole.RecruitmentOfficer,
+		"commissioned_officer" => ClanRole.CommissionedOfficer,
+		"officer"              => ClanRole.Officer,
+		"private"              => ClanRole.Private,
+		_                      => ClanRole.Unknown
+	};
+	
+	public static string ToInternalString(this ClanRole value) => value switch
+	{
+		ClanRole.Commander           => "commander",
+		ClanRole.ExecutiveOfficer    => "executive_officer",
+		ClanRole.RecruitmentOfficer  => "recruitment_officer",
+		ClanRole.CommissionedOfficer => "commissioned_officer",
+		ClanRole.Officer             => "officer",
+		ClanRole.Private             => "private",
+		_                            => "unknown"
+	};
+}
